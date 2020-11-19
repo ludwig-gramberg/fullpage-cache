5100722d09c88bf89604efa888629d71cd9d02a8
Ludwig Gramberg <info@ludwig-gramberg.de>
2020-11-19 19:50:26 +0100

    full config via settings

--- lib/FullPageCache/CacheServiceProvider.php
+++ lib/FullPageCache/CacheServiceProvider.php
@@ -14,7 +14,23 @@ class CacheServiceProvider implements ProviderInterface {
                 $domains = preg_split('/[\s]*,[\s]*/', $settings['domains'], -1, PREG_SPLIT_NO_EMPTY);
                 $schemes = preg_split('/[\s]*,[\s]*/', $settings['schemes'], -1, PREG_SPLIT_NO_EMPTY);
 
-                $config = new Config($domains, $schemes);
+                // defaults
+                
+                $defaultRefreshInterval = 600; 
+                $expireInterval = 600; 
+                $cacheClientFetchTimeout = 30;
+                
+                if(array_key_exists('refresh_interval', $settings) && preg_match('/^[1-9][0-9]*$/', $settings['refresh_interval'])) {
+                    $defaultRefreshInterval = intval($settings['refresh_interval']);
+                }
+                if(array_key_exists('expire_interval', $settings) && preg_match('/^[1-9][0-9]*$/', $settings['expire_interval'])) {
+                    $expireInterval = intval($settings['expire_interval']);
+                }
+                if(array_key_exists('fetch_timeout', $settings) && preg_match('/^[1-9][0-9]*$/', $settings['fetch_timeout'])) {
+                    $cacheClientFetchTimeout = intval($settings['fetch_timeout']);
+                }
+                
+                $config = new Config($domains, $schemes, $defaultRefreshInterval, $expireInterval, $cacheClientFetchTimeout);
 
                 if(array_key_exists('ignore_ssl_errors', $settings) && $settings['ignore_ssl_errors']) {
                     $config->setCacheClientIgnoreSslErrors(true);
