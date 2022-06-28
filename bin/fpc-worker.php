700fa5f032d2aa5dcbc37693a8c433a545081566
Ludwig Gramberg <info@ludwig-gramberg.de>
2022-06-28 13:28:20 +0200

    revision-file option

--- bin/fpc-worker.php
+++ bin/fpc-worker.php
@@ -5,13 +5,15 @@ namespace FullPageCache;
 
 use Webframework\Application\DependencyManager;
 use Webframework\Application\Settings;
+use Webframework\FileSystem\File;
 
 $usageHelp = <<<HELP
 
-usage: php fpc-worker.php [-h] [-v] [-d=<path>]
+usage: php fpc-worker.php [-h] [-v] [-d=<path>] [-r=<path>]
   -h show this help
   -v verbose, print extra output and errors
   -d=<path> app-directory, must point to project root 
+  -r=<path> path to revision-file to detect deployments
 
 example:
   php fpc-worker.php
@@ -24,6 +26,7 @@ $options = getopt('hvd::');
 $showHelp = array_key_exists('h', $options);
 $isVerbose = array_key_exists('v', $options);
 $appDirectory = array_key_exists('d', $options) ? $options['d'] : null;
+$revisionFilePath = array_key_exists('r', $options) ? $options['r'] : null;
 
 if(!$appDirectory) {
     // guess path if none was provided
@@ -33,6 +36,11 @@ if(!$appDirectory) {
 if(!$appDirectory || $showHelp) {
     echo $usageHelp;
     exit;
+}
+
+$revisionFile = null;
+if($revisionFilePath && is_file($revisionFilePath)) {
+    $revisionFile = new File($revisionFilePath);
 }
 
 require_once $appDirectory.'/vendor/ludwig-gramberg/webframework/bin/wf-cli-bootstrap.php';
@@ -60,7 +68,7 @@ try {
     $backend = $di->getInstance('FullPageCache\\Backend'); /* @var $backend Backend */
 
     // init worker
-    $worker = new CacheWorker($config, $backend, 'fpc-cache-worker-'.getmypid(), .2, null, 128);
+    $worker = new CacheWorker($config, $backend, 'fpc-cache-worker-'.getmypid(), .2, $revisionFile, 128);
     $worker->run();
     $worker->shutdown();
 
